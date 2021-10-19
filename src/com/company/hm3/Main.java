915758cee6e337e89f856a6952119c4d32c0fe33
Artem Degtjarenko <degtjarenko.artem2508@gmail.com>
2021-10-19 15:48:56 +0300

    Initial commit

---
+++ src/com/company/hm3/Main.java
@@ -0,0 +1,13 @@
+package com.company.hm3;
+
+public class Main {
+    public static void main(String[] args) {
+        Person egor = new Person();
+        Person anton = new Person();
+        Person max = new Person();
+
+
+        System.out.println(egor);
+        System.out.println(anton);
+    }
+}
---
+++ src/com/company/hm3/Person.java
@@ -0,0 +1,10 @@
+package com.company.hm3;
+
+public class Person {
+    private String name;
+    private String lastname;
+    private String fathername;
+
+
+
+}
