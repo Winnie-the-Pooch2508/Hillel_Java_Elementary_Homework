fc6512107a7da07c09b2a3f92af5f447fd0f0838
Artem Degtjarenko <degtjarenko.artem2508@gmail.com>
2021-10-19 16:09:23 +0300

    Initial commit

--- src/com/company/hm3/Person.java
+++ src/com/company/hm3/Person.java
@@ -5,6 +5,13 @@ public class Person {
     private String lastname;
     private String fathername;
 
-
-
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", fathername='" + fathername + '\'' +
+                '}';
+    }
+}
 }
