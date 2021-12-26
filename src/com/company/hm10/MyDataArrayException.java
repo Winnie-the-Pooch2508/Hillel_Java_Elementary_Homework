d81cc8278d755b507bdaccf55c7c3e632b97070c
Artem Degtjarenko <degtjarenko.artem2508@gmail.com>
2021-12-26 15:27:11 +0200

    hm10

---
+++ src/com/company/hm10/MyDataArrayException.java
@@ -0,0 +1,11 @@
+package com.company.hm10;
+
+public class MyDataArrayException extends RuntimeException {
+    public MyDataArrayException(String message) {
+        super(message);
+    }
+
+    public MyDataArrayException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
