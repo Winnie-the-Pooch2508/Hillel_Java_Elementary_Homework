68e6be9f93004276aa1f513c87f811ea768180aa
Artem Degtjarenko <degtjarenko.artem2508@gmail.com>
2022-01-03 00:34:18 +0200

    hm9

---
+++ src/com/company/hm9/CoordinateFinder.java
@@ -0,0 +1,5 @@
+package com.company.hm9;
+
+public interface CoordinateFinder {
+    MoveResult findCoordinate();
+}
---
+++ src/com/company/hm9/Main.java
@@ -0,0 +1,7 @@
+package com.company.hm9;
+
+public class Main {
+    public static void main(String[] args) {
+        new TicTacToe().start();
+    }
+}
