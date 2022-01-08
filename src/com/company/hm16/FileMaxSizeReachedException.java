2ac83b0041734a29494393cd8c92a00a2f5d4248
Artem Degtjarenko <degtjarenko.artem2508@gmail.com>
2022-01-08 12:34:47 +0200

    hm16

---
+++ src/com/company/hm16/FileMaxSizeReachedException.java
@@ -0,0 +1,8 @@
+package com.company.hm16;
+import java.io.IOException;
+
+public class FileMaxSizeReachedException {
+    public FileMaxSizeReachedException(String message) {
+        super(message);
+    }
+}
