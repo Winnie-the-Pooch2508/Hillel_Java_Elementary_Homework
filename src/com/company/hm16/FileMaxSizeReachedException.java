02626eafd57ffc0e2a85c250600a31edff107f0a
Winnie_The_Pooh <92647892+Winnie-the-Pooch2508@users.noreply.github.com>
2022-02-16 23:15:29 +0200

    Delete src/com/company/hm16/directory

--- src/com/company/hm16/FileMaxSizeReachedException.java
+++
@@ -1,7 +0,0 @@
-package com.company.hm16;
-import java.io.IOException;
-public class FileMaxSizeReachedException extends IOException {
-    public FileMaxSizeReachedException(String message) {
-        super(message);
-    }
-}
--- src/com/company/hm16/LoggingLevel.java
+++
@@ -1,5 +0,0 @@
-package com.company.hm16;
-public enum LoggingLevel {
-    INFO,
-    DEBUG,
-}
